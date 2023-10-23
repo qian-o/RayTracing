80ef1b6e898a5e24c9ddf7a96f11ffd41642bcc5
熙 王 <1324771795@qq.com>
2023-10-23 22:34:09 +0800

    统计运行时间

--- InOneWeekend/Program.cs
+++ InOneWeekend/Program.cs
@@ -9,9 +9,13 @@ internal class Program
     {
         _ = args;
 
+        DateTime beginTime = DateTime.Now;
+
         IChapter chapter = new Chapter14();
 
         chapter.Run();
+
+        Console.WriteLine($"Total time: {DateTime.Now - beginTime}");
 
         Console.ReadKey();
     }
